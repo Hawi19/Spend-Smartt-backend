97cb0eec4c1bb38aa7b9384441f9eb0703c45098
Hawi19 <hawigirma1996@gmail.com>
2025-02-11 02:27:15 +0300

    mailer.js updatesd

--- mailer.js
+++ mailer.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 export const sendVerificationEmail = async (email, token) => {
-  const verificationLink = `http://localhost:5173/verify?token=${token}`;
+  const verificationLink = `https://spend-smart-frontend.vercel.app//verify?token=${token}`;
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
